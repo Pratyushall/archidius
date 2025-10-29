2a8f196db79a501864527fedb19f96254e6c2dd8
Pratyushall <praty0sha8@gmail.com>
2025-10-29 19:13:44 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // ⚠️ Allows production builds to succeed even with ESLint errors
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // ⚠️ Allows production builds to succeed even with TS type errors
+    ignoreBuildErrors: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
